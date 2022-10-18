b271c8e5f3b995f943d93e079966840382fc0e8c
Peter Kingori <kpndegwa@gmail.com>
2022-10-18 20:37:51 +0300

    make the drawing by a user remain on the screen.

--- app/src/main/java/com/pkndegwa/kiddiedrawing/DrawingView.kt
+++ app/src/main/java/com/pkndegwa/kiddiedrawing/DrawingView.kt
@@ -14,6 +14,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mBrushSize: Float = 0.0f
     private var color = Color.BLACK
     private var canvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
 
     init {
         setupDrawing()
@@ -39,6 +40,13 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawBitmap(mCanvasBitmap!!, 0.0f, 0.0f, mCanvasPaint)
+
+        for (path in mPaths) {
+            mDrawPaint?.strokeWidth  = path.brushThickness
+            mDrawPaint?.color = path.color
+            canvas?.drawPath(path, mDrawPaint!!)
+        }
+
         if (!mDrawPath?.isEmpty!!) {
             mDrawPaint?.strokeWidth  = mDrawPath!!.brushThickness
             mDrawPaint?.color = mDrawPath!!.color
@@ -61,6 +69,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
                 mDrawPath!!.lineTo(touchX!!, touchY!!)
             }
             MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
