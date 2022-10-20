79dc0505e297f65a30d32a2d5cd0cd92b85b8458
Peter Kingori <kpndegwa@gmail.com>
2022-10-20 22:42:19 +0300

    add a custom dialog and functionality to allow the user to select different brush sizes.

--- app/src/main/java/com/pkndegwa/kiddiedrawing/MainActivity.kt
+++ app/src/main/java/com/pkndegwa/kiddiedrawing/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.pkndegwa.kiddiedrawing
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import com.pkndegwa.kiddiedrawing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,35 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = binding.drawingView
         drawingView!!.setBrushSize(20.0.toFloat())
+
+        val brushImageButton = binding.brushImageButton
+        brushImageButton.setOnClickListener {
+            selectBrushSizeDialog()
+        }
+    }
+
+    private fun selectBrushSizeDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+
+        val smallBrush = brushDialog.findViewById<ImageButton>(R.id.small_brush_image_button)
+        smallBrush.setOnClickListener {
+            drawingView?.setBrushSize(10.0.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val mediumBrush = brushDialog.findViewById<ImageButton>(R.id.medium_brush_image_button)
+        mediumBrush.setOnClickListener {
+            drawingView?.setBrushSize(20.0.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeBrush = brushDialog.findViewById<ImageButton>(R.id.large_brush_image_button)
+        largeBrush.setOnClickListener {
+            drawingView?.setBrushSize(30.0.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
